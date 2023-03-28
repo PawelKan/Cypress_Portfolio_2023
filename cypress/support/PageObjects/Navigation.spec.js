e3240f76a75cceba4a537b209c70259d51010d73
Paweł K <grylion02@gmail.com>
2023-03-28 16:31:04 +0200

    add contact us page main elements check

--- cypress/support/PageObjects/Navigation.spec.js
+++ cypress/support/PageObjects/Navigation.spec.js
@@ -4,6 +4,7 @@ class NavigateToUrl{
 
     get homePageUrl() { return cy.visit("/") }
     get loginPageUrl() { return cy.visit("/login") }
+    get contactUsUrl() {return cy.visit ("/contact_us")}
 
 }
 export const navigateTo = new NavigateToUrl();
